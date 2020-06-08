698f6c34e3b22afdbff4080438a0c09dcb21171f
Daomtthuan <dao.mt.thuan@gmail.com>
2020-06-08 20:44:24 +0700

    create bao-chi.php

---
+++ pages/bao-chi.php
@@ -0,0 +1,63 @@
+<?php
+$news = [
+  0 => [
+    'title' => '[VTV1] Chia sẻ lộ trình với người khác',
+    'info'  => 'Hiện thực hóa 1 ý tưởng đã thành công không dễ khi thị trường có nhiều sự khác biệt. Cùng tìm hiểu ngay!',
+  ],
+  2 => [
+    'title' => 'Hành trình đi chung taxi về quê',
+    'info'  => 'Chương trình "Hành trình 24h" được phát xong (12h55) ngày 03/02/2016 đề cập đến vấn đề đi chung taxi về quê.',
+  ],
+  3 => [
+    'title' => '[VTC2] Phóng sự đi chung xe taxi Nội Bài',
+    'info'  => 'Đi chung xe ngoài việc giảm ùn tắc, tiết kiệm chi phí, bảo vệ môi trường, đi chung xe còn là cơ hội để tìm kiếm bạn bè, đối tác...',
+  ],
+  4 => [
+    'title' => '[VTC10] Lợi ích từ việc đi chung xe',
+    'info'  => 'Chuyên mục Nhịp sống mới - VTC10 có phóng sự về hình thức đi chung xe và những lợi ích từ việc đi chung xe mang lại cho mỗi cá nhân.',
+  ],
+  5 => [
+    'title' => '[VTV1] Đi Chung xe taxi tiết kiệm chi phí',
+    'info'  => 'Phóng sự về đi chung taxi chuyên tuyến Hà Nội - Nội Bài do Ban thời sự - VTV1 thực hiện. Phát sóng lúc 17h45 ngày 26/11/2013.',
+  ],
+  6 => [
+    'title' => '[HTV1] Đi chung taxi Hà Nội - Nội Bài',
+    'info'  => 'Phóng sự về đi chung xe được phát trên chuyên mục "7 Ngày công nghệ" do VTV2 - Ban khoa giáo, Đài truyền hình Việt Nam thực hiện.',
+  ],
+];
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <title>Báo chí</title>
+</head>
+
+<body>
+  <h1 class="text-center">Báo chí</h1>
+
+  <div class="container">
+    <div>
+      <form>
+        <div class="form-group text-center">
+          <input type="search" class="form-control" placeholder="Tìm kiếm bài viết">
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- test.php
+++
@@ -1,3 +0,0 @@
-<?php
-$a = 1;
-echo "test thử xem nào: $a + $a" . ($a + $a);
