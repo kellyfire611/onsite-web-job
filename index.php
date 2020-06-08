e72791fa5798919e328b32524fc5cdcabf7d40cc
Khanh <gawe0405@gmail.com>
2020-06-08 20:55:45 +0700

    Khanh gắn link

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Xin chào Khanh đã chỉnh sửa web này!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/pages/chinh-sach-van-tai.php">Khanh làm chính sách vận tải</a>
+</body>
+</html>
