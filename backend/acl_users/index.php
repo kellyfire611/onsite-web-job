e123196a3d6512e007fee097611ac38861141b12
Daomtthuan <dao.mt.thuan@gmail.com>
2020-06-20 09:05:28 +0700

    refs #86: add index.php of /backend/acl_users/

--- backend/acl_users/index.php
+++ backend/acl_users/index.php
@@ -2,26 +2,65 @@
 require_once __DIR__ . '/../../bootstrap.php';
 include_once __DIR__ . '/../../dbconnect.php';
 
-$sql = "select * from `shop_suppliers`";
-$result = mysqli_query($conn, $sql);
+$query = <<<query
+select
+  id,
+  username,
+  last_name,
+  first_name,
+  email,
+  avatar,
+  job_title,
+  department,
+  manager_id,
+  phone,
+  address1,
+  address2,
+  city,
+  state,
+  postal_code,
+  country,
+  remember_token,
+  active_code,
+  status,
+  created_at,
+  updated_at
+from acl_users
+query;
 
-$data   = [];
-$rowNum = 1;
+$result = mysqli_query($conn, $query);
+
+$data  = [];
+$index = 0;
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-  $data[] = array(
-    'rowNum'        => $rowNum, // sử dụng biến tự tăng để làm dữ liệu cột STT
-    'supplier_code' => $row['supplier_code'],
-    'supplier_name' => $row['supplier_name'],
-    'description'   => $row['description'],
-    'image'         => $row['image'],
-    'created_at'    => $row['created_at'],
-    'updated_at'    => $row['updated_at'],
-  );
-  $rowNum++;
+  $data[] = [
+    'no'             => ++$index,
+    'id'             => $row['id'],
+    'username'       => $row['username'],
+    'name'           => [
+      'first' => $row['first_name'],
+      'last'  => $row['last_name'],
+    ],
+    'email'          => $row['email'],
+    'avatar'         => $row['avatar'],
+    'job_title'      => $row['job_title'],
+    'department'     => $row['department'],
+    'manager_id'     => $row['manager_id'],
+    'phone'          => $row['phone'],
+    'address1'       => $row['address1'],
+    'address2'       => $row['address2'],
+    'city'           => $row['city'],
+    'state'          => $row['state'],
+    'postal_code'    => $row['postal_code'],
+    'country'        => $row['country'],
+    'remember_token' => $row['remember_token'],
+    'active_code'    => $row['active_code'],
+    'status'         => $row['status'],
+    'created_at'     => $row['created_at'],
+    'updated_at'     => $row['updated_at'],
+  ];
 }
 
-// Yêu cầu `Twig` vẽ giao diện được viết trong file `backend/shop_suppliers/index.html.twig`
-// với dữ liệu truyền vào file giao diện được đặt tên là `suppliers`
-echo $twig->render('backend/shop_suppliers/index.html.twig', [
-  'suppliers' => $data,
+echo $twig->render('backend/acl_users/index.html.twig', [
+  'data' => json_encode($data),
 ]);
