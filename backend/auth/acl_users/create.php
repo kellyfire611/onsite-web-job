76acc34ea3ff1504608d73d5f589e7be2d85e849
Daomtthuan <dao.mt.thuan@gmail.com>
2020-06-22 09:40:59 +0700

    update ui create

--- backend/auth/acl_users/create.php
+++ backend/auth/acl_users/create.php
@@ -1,112 +1,95 @@
 <?php
 require_once __DIR__ . '/../../../bootstrap.php';
 
+$regex = [
+  'username'    => [
+    'pattern' => Config::$REGEX_PATTERN['username'],
+    'min'     => 5,
+    'max'     => 191,
+  ],
+  'last_name'   => [
+    'pattern' => Config::$REGEX_PATTERN['vietnamese_name'],
+    'min'     => 1,
+    'max'     => 255,
+  ],
+  'first_name'  => [
+    'pattern' => Config::$REGEX_PATTERN['vietnamese_name'],
+    'min'     => 1,
+    'max'     => 255,
+  ],
+  'email'       => [
+    'pattern' => Config::$REGEX_PATTERN['email'],
+    'min'     => 12,
+    'max'     => 191,
+  ],
+  'job_title'   => [
+    'pattern' => Config::$REGEX_PATTERN['any'],
+    'min'     => 0,
+    'max'     => 255,
+  ],
+  'department'  => [
+    'pattern' => Config::$REGEX_PATTERN['any'],
+    'min'     => 0,
+    'max'     => 255,
+  ],
+  'phone'       => [
+    'pattern' => Config::$REGEX_PATTERN['phone'],
+    'min'     => 10,
+    'max'     => 25,
+  ],
+  'address1'    => [
+    'pattern' => Config::$REGEX_PATTERN['any'],
+    'min'     => 0,
+    'max'     => 500,
+  ],
+  'address2'    => [
+    'pattern' => Config::$REGEX_PATTERN['any'],
+    'min'     => 0,
+    'max'     => 500,
+  ],
+  'city'        => [
+    'pattern' => Config::$REGEX_PATTERN['vietnamese_name'],
+    'min'     => 0,
+    'max'     => 255,
+  ],
+  'state'       => [
+    'pattern' => Config::$REGEX_PATTERN['vietnamese_name'],
+    'min'     => 0,
+    'max'     => 255,
+  ],
+  'postal_code' => [
+    'pattern' => Config::$REGEX_PATTERN['number'],
+    'min'     => 0,
+    'max'     => 15,
+  ],
+  'country'     => [
+    'pattern' => Config::$REGEX_PATTERN['vietnamese_name'],
+    'min'     => 0,
+    'max'     => 255,
+  ],
+];
+
 if (isset($_POST['submit'])) {
   include_once __DIR__ . '/../../../dbconnect.php';
 
-  // Lấy dữ liệu người dùng hiệu chỉnh gởi từ REQUEST POST
-  $tenLoai = $_POST['lsp_ten'];
-  $mota    = $_POST['lsp_mota'];
-
-  // Câu lệnh INSERT
-  $sql = "INSERT INTO `loaisanpham` (lsp_ten, lsp_mota) VALUES ('" . $tenLoai . "', '" . $mota . "');";
-
-  // Thực thi INSERT
-  mysqli_query($conn, $sql);
-
-  // Đóng kết nối
-  mysqli_close($conn);
-
-  header('location:./');
+$target_dir    = "uploads/";
+$target_file   = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk      = 1;
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if ($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
 } else {
-  $fields = [
-    0  => [
-      'key'      => 'username',
-      'label'    => 'Tài khoản',
-      'sortable' => true,
-      'class'    => 'align-middle',
-    ],
-    1 => [
-      'key'      => 'last_name',
-      'label'    => 'Họ',
-      'sortable' => true,
-      'class'    => 'd-none',
-    ],
-    2  => [
-      'key'      => 'first_name',
-      'label'    => 'Tên',
-      'sortable' => true,
-      'class'    => 'd-none',
-    ],
-    3  => [
-      'key'      => 'email',
-      'label'    => 'Email',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none d-xl-table-cell',
-    ],
-    4  => [
-      'key'   => 'avatar',
-      'label' => 'Ảnh đại diện',
-      'class' => 'd-none',
-    ],
-    5  => [
-      'key'      => 'job_title',
-      'label'    => 'Công việc',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none',
-    ],
-    6  => [
-      'key'      => 'department',
-      'label'    => 'Bộ phận',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none d-xl-table-cell',
-    ],
-    7 => [
-      'key'      => 'phone',
-      'label'    => 'Điện thoại',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none',
-    ],
-    8 => [
-      'key'      => 'address1',
-      'label'    => 'Địa chỉ 1',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none',
-    ],
-    9 => [
-      'key'      => 'address2',
-      'label'    => 'Địa chỉ 2',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none',
-    ],
-    10 => [
-      'key'      => 'city',
-      'label'    => 'Thành phố',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none',
-    ],
-    11 => [
-      'key'      => 'state',
-      'label'    => 'Bang',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none',
-    ],
-    12 => [
-      'key'      => 'postal_code',
-      'label'    => 'Mã bưu điện',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none',
-    ],
-    17 => [
-      'key'      => 'country',
-      'label'    => 'Quốc gia',
-      'sortable' => true,
-      'class'    => 'align-middle d-block d-md-none',
-    ],
-  ];
-
   echo $twig->render('backend/auth/acl_users/create.html.twig', [
     'el'   => 'form-create',
-    'data' => json_encode([])
+    'data' => json_encode($regex),
   ]);
 }
--- config.php
+++ config.php
@@ -1,29 +1,38 @@
 <?php
-class Config
-{
-    public static $DB_CONNECTION_HOST           = '156.67.222.148';
-    public static $DB_CONNECTION_USERNAME       = 'u883604362_taxi';
-    public static $DB_CONNECTION_PASSWORD       = 'Db@#Taxi123';
-    public static $DB_CONNECTION_DATABASE_NAME  = 'u883604362_taxi';
+class Config {
+  public static $DB_CONNECTION_HOST          = '156.67.222.148';
+  public static $DB_CONNECTION_USERNAME      = 'u883604362_taxi';
+  public static $DB_CONNECTION_PASSWORD      = 'Db@#Taxi123';
+  public static $DB_CONNECTION_DATABASE_NAME = 'u883604362_taxi';
 
-    public static $LIMIT = 5;
-    public static $PAGE = 1;
+  public static $LIMIT = 5;
+  public static $PAGE  = 1;
 
-    public static $BOOTSTRAP_RATING_CLASS = array(
-        5 => array(
-            'class' => 'bg-success',    // 5 sao, class quy định màu mặc định (Bootstrap)
-        ),
-        4 => array(
-            'class' => 'bg-primary',    // 4 sao, class quy định màu mặc định (Bootstrap)
-        ),
-        3 => array(
-            'class' => 'bg-info',       // 3 sao, class quy định màu mặc định (Bootstrap)
-        ),
-        2 => array(
-            'class' => 'bg-warning',    // 2 sao, class quy định màu mặc định (Bootstrap)
-        ),
-        1 => array(
-            'class' => 'bg-danger',     // 1 sao, class quy định màu mặc định (Bootstrap)
-        ),
-    );
+  public static $BOOTSTRAP_RATING_CLASS = array(
+    5 => array(
+      'class' => 'bg-success', // 5 sao, class quy định màu mặc định (Bootstrap)
+    ),
+    4 => array(
+      'class' => 'bg-primary', // 4 sao, class quy định màu mặc định (Bootstrap)
+    ),
+    3 => array(
+      'class' => 'bg-info', // 3 sao, class quy định màu mặc định (Bootstrap)
+    ),
+    2 => array(
+      'class' => 'bg-warning', // 2 sao, class quy định màu mặc định (Bootstrap)
+    ),
+    1 => array(
+      'class' => 'bg-danger', // 1 sao, class quy định màu mặc định (Bootstrap)
+    ),
+  );
+
+  public static $REGEX_PATTERN = [
+    'username'        => '^\w+$',
+    'vietnamese_name' => '^[a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]+( [a-zA-ZàáãạảăắằẳẵặâấầẩẫậèéẹẻẽêềếểễệđìíĩỉịòóõọỏôốồổỗộơớờởỡợùúũụủưứừửữựỳỵỷỹýÀÁÃẠẢĂẮẰẲẴẶÂẤẦẨẪẬÈÉẸẺẼÊỀẾỂỄỆĐÌÍĨỈỊÒÓÕỌỎÔỐỒỔỖỘƠỚỜỞỠỢÙÚŨỤỦƯỨỪỬỮỰỲỴỶỸÝ]+)*$',
+    'email'           => '^[a-z][a-z0-9_\\.]{5,32}@[a-z0-9]{2,}(\\.[a-z0-9]{2,4}){1,3}$',
+    'date'            => '^([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))$',
+    'phone'           => '^\\+?\\d{10,}$',
+    'any'             => '^.+$',
+    'number'          => '^-?\\d+$',
+  ];
 }
